85d9417e881ef67cb626accd61ed751a48137c4b
Derlinuss <yarayansedat@gmail.com>
2026-07-13 14:26:25 +0300

    Fix Render deployment

--- server/src/config/firebase.ts
+++ server/src/config/firebase.ts
@@ -1,11 +1,15 @@
-import { initializeApp, cert } from "firebase-admin/app";
+import { initializeApp, cert, getApps } from "firebase-admin/app";
 import { getAuth } from "firebase-admin/auth";
+import * as fs from "fs";
+import * as path from "path";
 
-const serviceAccount = require("../../serviceAccountKey.json");
+if (!getApps().length) {
+  const keyPath = path.join(__dirname, "../../serviceAccountKey.json");
+  if (fs.existsSync(keyPath)) {
+    initializeApp({ credential: cert(require(keyPath)) });
+  } else {
+    initializeApp();
+  }
+}
 
-const app = initializeApp({
-  credential: cert(serviceAccount),
-});
-
-export const auth = getAuth(app);
-export default app;
+export const auth = getAuth();
