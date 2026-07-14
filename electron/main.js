0d65c9c4a2f649de24e209aedbd6399c492481f1
Derlinuss <yarayansedat@gmail.com>
2026-07-14 18:05:13 +0300

    update #1

---
+++ electron/main.js
@@ -0,0 +1,32 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1280,
+    height: 800,
+    minWidth: 800,
+    minHeight: 500,
+    title: "Parax",
+    icon: path.join(__dirname, "Parax.png"),
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+      contextIsolation: true,
+      nodeIntegration: false,
+    },
+  });
+
+  win.loadURL("https://parax-vqqb.onrender.com");
+
+  win.on("page-title-updated", (e) => e.preventDefault());
+}
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
