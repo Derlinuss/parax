c2aa5141bc9f373e7ea937fe8384f7af10af8306
Derlinuss <yarayansedat@gmail.com>
2026-07-19 13:06:55 +0300

    Track monitoring pages, add simple password protection

---
+++ public/password.js
@@ -0,0 +1,15 @@
+const PROTECTED_PASSWORD = "ParaxOfficial_2026_Secure#Strong!";
+
+function checkPassword() {
+    if (sessionStorage.getItem("authenticated") === "true") return;
+
+    const password = prompt("Enter password to access monitoring:");
+    if (password === PROTECTED_PASSWORD) {
+        sessionStorage.setItem("authenticated", "true");
+    } else {
+        alert("Access Denied");
+        window.location.href = "/";
+    }
+}
+
+checkPassword();
